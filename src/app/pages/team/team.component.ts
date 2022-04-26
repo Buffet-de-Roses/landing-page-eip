8eb6f9e9c514147e1e12c8bb12f2103fb2ab3685
tom marescot <tom@icilaba.fr>
2022-04-26 15:10:18 +0200

    responsive mobile

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -26,36 +26,36 @@ export class TeamComponent implements OnInit {
     role: 'dev',
     imgUrl: 'https://via.placeholder.com/150x150',
   },
-  {
-    firstname: 'Yann',
-    lastname: 'Peu',
-    role: 'dev',
-    imgUrl: 'https://via.placeholder.com/150x150',
-  },
-  {
-    firstname: 'Bastien',
-    lastname: 'Garcia',
-    role: 'dev',
-    imgUrl: 'https://via.placeholder.com/150x150',
-  },
-  {
-    firstname: 'Karl-Edouard',
-    lastname: 'Gläser',
-    role: 'dev',
-    imgUrl: 'https://via.placeholder.com/150x150',
-  },
-  {
-    firstname: 'Bryan',
-    lastname: 'Sa',
-    role: 'dev',
-    imgUrl: 'https://via.placeholder.com/150x150',
-  },
-  {
-    firstname: 'Matteo',
-    lastname: 'De Faria',
-    role: 'dev',
-    imgUrl: 'https://via.placeholder.com/150x150',
-  }
+  // {
+  //   firstname: 'Yann',
+  //   lastname: 'Peu',
+  //   role: 'dev',
+  //   imgUrl: 'https://via.placeholder.com/150x150',
+  // },
+  // {
+  //   firstname: 'Bastien',
+  //   lastname: 'Garcia',
+  //   role: 'dev',
+  //   imgUrl: 'https://via.placeholder.com/150x150',
+  // },
+  // {
+  //   firstname: 'Karl-Edouard',
+  //   lastname: 'Gläser',
+  //   role: 'dev',
+  //   imgUrl: 'https://via.placeholder.com/150x150',
+  // },
+  // {
+  //   firstname: 'Bryan',
+  //   lastname: 'Sa',
+  //   role: 'dev',
+  //   imgUrl: 'https://via.placeholder.com/150x150',
+  // },
+  // {
+  //   firstname: 'Matteo',
+  //   lastname: 'De Faria',
+  //   role: 'dev',
+  //   imgUrl: 'https://via.placeholder.com/150x150',
+  // }
 ]
   constructor() { }
 
